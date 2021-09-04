b516e02acf9f8b97891af6937fc5517a7298ba80
Jayanek <jayanekwije123@gmail.com>
2021-09-04 12:42:00 +0530

    update model

--- ml_components.py
+++ ml_components.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
-import tensorflow_hub as hub
+# import tensorflow_hub as hub custom_objects={'KerasLayer':hub.KerasLayer}
 import numpy as np
-
+from tensorflow.keras.applications import EfficientNetB2
 from tensorflow.keras.models import load_model
 from PIL import Image
 
 
-target_names=['aloperia areata','beau\'s lines','Darier\'s disease','Muehrck-e\'s lines']
+target_names=['terry\'s nail','beau\'s lines','splinter hemmorrage','yellow nails','healthy nails']
 
 def load_the_model():
-    model = load_model('basic_model.h5',custom_objects={'KerasLayer':hub.KerasLayer})
+    model = load_model('model_l48_a838.h5')
     print("Model loaded")
     return model
 
@@ -19,7 +19,7 @@ model = load_the_model()
 def image_preprocess(image: Image.Image)-> Image.Image:
 	image = np.asarray(image.resize((224, 224)))[..., :3]
 	image = np.expand_dims(image, 0)
-	image = image / 255.0
+	#image = image / 255.0
 	return image
 
 
